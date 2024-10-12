1d8a392007d402317e44cf5a5910ec6e73517bb4
K Dinesh Karthikeyan <kdineshkarthikeyan@gmail.com>
2024-10-12 19:36:31 +0530

    creating docload schema

--- main_server/schema.py
+++ main_server/schema.py
@@ -1,10 +1,16 @@
 from pydantic import BaseModel
 
+
 class Chat(BaseModel):
     query: str
     document_id: str | None = None
     agent_id: str | None = None
 
+
 class ChatResponse(BaseModel):
     bot_message: str
     reference: str | None = None
+
+
+class DocLoad(BaseModel):
+    doc_id: str
