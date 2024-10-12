837da927d5959ee9e70d5e8bcf216ab6429b3676
K Dinesh Karthikeyan <kdineshkarthikeyan@gmail.com>
2024-10-13 04:53:33 +0530

    using ollama local models

--- main_server/main.py
+++ main_server/main.py
@@ -7,17 +7,18 @@ from utils import (
     save_and_verify_docs,
 )
 from llama_deploy import LlamaDeployClient, ControlPlaneConfig
+from llama_index.llms.ollama import Ollama
 
 client = LlamaDeployClient(ControlPlaneConfig())
 
 app = FastAPI()
 
+
 @app.post("/upload_document")
 async def upload_documents(
     file: UploadFile,
-    mobile_id: Annotated[str | None , Header()],
+    mobile_id: Annotated[str | None, Header()],
 ):
-
     file_name = file.filename
     file_content_type = file.content_type
     content = await file.read()
@@ -32,17 +33,15 @@ async def upload_documents(
 
 @app.post("/chat")
 async def chat_llama(
-    chat: Chat,
-    mobile_id: Annotated[str | None, Header()]
+    chat: Chat, mobile_id: Annotated[str | None, Header()]
 ) -> ChatResponse:
-
     session = client.get_or_create_session(session_id=mobile_id)
 
     query = chat.query
 
     if chat.document_id is not None:
         response = session.run("security_layer_agent", query=query)
-        
+
         if response == "granted":
             # response, reference = session.run("docs_agent", query=query, document_id=chat.document_id)
             return ChatResponse(bot_message=query, reference=query)
@@ -50,7 +49,7 @@ async def chat_llama(
             response = "I'm designed to help with order-related tasks and questions about user documents. Unfortunately, I can't assist with your current request. Feel free to ask me something else within those parameters."
     else:
         response = session.run("security_layer_agent", query=query)
-        
+
         if response == "granted":
             if chat.agent_id == "order_agent":
                 response = session.run("order_agent", query=query)
@@ -64,5 +63,5 @@ async def chat_llama(
                 pass
         else:
             response = "I'm designed to help with order-related tasks and questions about user documents. Unfortunately, I can't assist with your current request. Feel free to ask me something else within those parameters."
-            
+
     return ChatResponse(bot_message=response)
