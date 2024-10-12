683c981606fc1ca7aa8fbf7006e2a61c55046e60
RionDsilvaCS <dsilvarion@gmail.com>
2024-10-12 14:33:57 +0530

    add order workflow

---
+++ llama_deploy/workflows/order_workflow/agent.py
@@ -0,0 +1,32 @@
+from llama_index.core.agent import ReActAgent
+from llama_index.llms.ollama import Ollama
+from llama_index.core.tools import FunctionTool
+from agent_tool import (
+    create_order_tool,
+    view_orders_tool,
+    order_status_tool,
+)
+from agent_template import api_agent_prompt
+
+class OrderAgent:
+    def __init__(self) -> None:
+
+        # agent tools
+        _create_order_tool = FunctionTool.from_defaults(fn=create_order_tool)
+        _view_orders_tool = FunctionTool.from_defaults(fn=view_orders_tool)
+        _order_status_tool = FunctionTool.from_defaults(fn=order_status_tool)
+
+        # llm
+        _llm = Ollama(model="gemma2:2b", request_timeout=60.0)
+
+        # list of tools
+        _tools = [_create_order_tool, _view_orders_tool, _order_status_tool]
+
+        # initializing ReAct agent
+        self.agent = ReActAgent.from_tools(tools=_tools, llm=_llm, verbose=True)
+
+        # replacing system prompt for ReAct agent
+        self.agent.update_prompts({"agent_worker:system_prompt": api_agent_prompt})
+
+    def get_agent(self):
+        return self.agent
---
+++ llama_deploy/workflows/order_workflow/app.py
@@ -0,0 +1,40 @@
+from llama_index.core.workflow import (
+    StartEvent,
+    StopEvent,
+    Workflow,
+    step,
+    Event,
+    Context,
+)
+from llama_index.llms.ollama import Ollama
+from llama_index.utils.workflow import draw_all_possible_flows
+from agent import OrderAgent
+
+order_agent_obj = OrderAgent()
+order_agent = order_agent_obj.get_agent()
+
+class OrderWorkflow(Workflow):
+    @step
+    async def step_two(self, ctx: Context, ev: StartEvent) -> StopEvent:
+        
+        query = f"<warning> Utilize the tools first to execute the query</warning>\n <query> {ev.query} </query>\n <instruction> Humanize the tool output and don't specify API details </instruction>"
+        response = order_agent.chat(query)
+
+        return StopEvent(result=response)
+
+
+async def main():
+    w = OrderWorkflow(timeout=120, verbose=True)
+    # result = await w.run(query="""Create a order for product id "BAIDN31" and the product name is "TITAN watch" """)
+    # result = await w.run(query=""" View all the current orders """)
+    result = await w.run(query=""" View the status of the order with order id "7015" """)
+
+    print(result)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
+
+    # draw_all_possible_flows(OrderWorkflow, filename="order_workflow.html")
