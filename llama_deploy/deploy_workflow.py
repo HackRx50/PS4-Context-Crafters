8a1c8b88395476972e4c3508ddd6f163374fa869
RionDsilvaCS <dsilvarion@gmail.com>
2024-10-12 11:53:05 +0530

    initial commit | add directory structure

---
+++ llama_deploy/deploy_workflow.py
@@ -0,0 +1,14 @@
+from llama_deploy import (
+    deploy_workflow,
+    WorkflowServiceConfig,
+    ControlPlaneConfig,
+)
+import asyncio
+from llama_index.core.workflow import Workflow, StartEvent, StopEvent, step
+
+async def main():
+    pass
+    
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
